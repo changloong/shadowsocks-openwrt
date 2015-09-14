714b8d8b2c9a50f1a44399d6c9e7761f74a7e095
Chang Long <changlon@gmail.com>
2015-09-14 11:54:48 +0800

    update dns config path

--- server.sh
+++ server.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 pidfile="/var/run/ss-server.pid"
 if [ -f "$pidfile" ]; then
-	pid=$(/bin/cat $pidfile)
-	/bin/kill $pid
+        pid=$(/bin/cat $pidfile)
+        /bin/kill $pid
 fi
 #echo "ss-server"
-/usr/bin/ss-server -c /etc/shadowsocks/server.json --acl /etc/shadowsocks/local.acl -f $pidfile
+/usr/bin/ss-server -c /etc/vpn/config.json --acl /etc/vpn/local.acl -f $pidfile
 
 pidfile="/var/run/ss-dns.pid"
 if [ -f "$pidfile" ]; then
         pid=$(/bin/cat $pidfile)
         /bin/kill $pid
 fi
-#echo "ss-redir" 
-/usr/bin/ss-redir -c /etc/shadowsocks/dns.json -b 0.0.0.0 -f $pidfile
+
+#echo "ss-redir"
+/usr/bin/ss-redir -c /etc/vpn/dns.json -b 0.0.0.0 -f $pidfile
 
 pidfile="/var/run/ss-local.pid"
 if [ -f "$pidfile" ]; then
         pid=$(/bin/cat $pidfile)
         /bin/kill $pid
 fi
+
 #echo "ss-local"
 /usr/bin/ss-local -c /var/etc/shadowsocks.json -b 192.168.0.1 -l 7777 --fast-open -u -f $pidfile
