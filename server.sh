ea8dc89fdfd74fcd00760efd9bddd0eadc85a227
Chang Long <changlon@gmail.com>
2015-09-14 21:36:04 +0800

    add cronjob task

--- server.sh
+++ server.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# openwrt Scheduled Tasks
+# minute(0-59) hour(0-23) month(1-31)  year(1-12) week(0-6) 
+# 0 3 * * * /etc/vpn/server.sh > /tmp/ss-server.log 2>&1
+
 echo "ss-server"
 pidfile="/var/run/ss-server.pid"
 if [ -f "$pidfile" ]; then
@@ -6,22 +11,6 @@ if [ -f "$pidfile" ]; then
         /bin/kill $pid
 fi
 /usr/bin/ss-server -c /etc/vpn/config.json --acl /etc/vpn/local.acl -f $pidfile
-
-#echo "ss-dns-redir"
-pidfile="/var/run/ss-dns-redir.pid"
-if [ -f "$pidfile" ]; then
-        pid=$(/bin/cat $pidfile)
-        /bin/kill $pid
-fi
-#/usr/bin/ss-redir -c /etc/vpn/dns.json -b 0.0.0.0 -f $pidfile
-
-#echo "ss-dns-tunnel"
-pidfile="/var/run/ss-dns-tunnel.pid"
-if [ -f "$pidfile" ]; then
-        pid=$(/bin/cat $pidfile)
-        /bin/kill $pid
-fi
-#/usr/bin/ss-tunnel -c /etc/vpn/dns.json -l 5353 -L 10.168.172.192:53 -f $pidfile -u
 
 echo "ss-local"
 pidfile="/var/run/ss-local.pid"
