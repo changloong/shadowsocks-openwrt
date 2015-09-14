fc02ee2508ca52f78e7a0148365364ad4bebce43
Chang Long <changlon@gmail.com>
2015-09-14 12:07:58 +0800

    update dns config path

--- server.sh
+++ server.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
+echo "ss-server"
 pidfile="/var/run/ss-server.pid"
 if [ -f "$pidfile" ]; then
         pid=$(/bin/cat $pidfile)
         /bin/kill $pid
 fi
-#echo "ss-server"
 /usr/bin/ss-server -c /etc/vpn/config.json --acl /etc/vpn/local.acl -f $pidfile
 
-pidfile="/var/run/ss-dns.pid"
+echo "ss-dns-redir"
+pidfile="/var/run/ss-dns-redir.pid"
 if [ -f "$pidfile" ]; then
         pid=$(/bin/cat $pidfile)
         /bin/kill $pid
 fi
-
-#echo "ss-redir"
 /usr/bin/ss-redir -c /etc/vpn/dns.json -b 0.0.0.0 -f $pidfile
 
+echo "ss-dns-tunnel"
+pidfile="/var/run/ss-dns-tunnel.pid"
+if [ -f "$pidfile" ]; then
+        pid=$(/bin/cat $pidfile)
+        /bin/kill $pid
+fi
+/usr/bin/ss-tunnel -c /etc/vpn/dns.json -l 5353 -L 10.168.172.192:53 -f $pidfile -u
+
+echo "ss-local"
 pidfile="/var/run/ss-local.pid"
 if [ -f "$pidfile" ]; then
         pid=$(/bin/cat $pidfile)
         /bin/kill $pid
 fi
-
-#echo "ss-local"
 /usr/bin/ss-local -c /var/etc/shadowsocks.json -b 192.168.0.1 -l 7777 --fast-open -u -f $pidfile
